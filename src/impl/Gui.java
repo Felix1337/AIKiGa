229b13e3701b11f1f8def2731ec357e54def8c3e
Felix Schmidt <felix.schmidt@haw-hamburg.de>
2012-10-11 18:38:32 +0200

    gui angepasst

--- src/impl/Gui.java
+++ src/impl/Gui.java
@@ -26,7 +26,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 
 import javax.swing.Action;
@@ -39,6 +43,7 @@ import javax.swing.AbstractListModel;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.SwingConstants;
 
 public class Gui {
 
@@ -48,8 +53,8 @@ public class Gui {
 	private JTextField textFieldGehalt;
 	private Logic l;
 	private ArrayList<Kita> kitas = new ArrayList<Kita>();
-	private ArrayList<Gruppe> gruppen = new ArrayList<Gruppe>();
-	private ArrayList<Kind> kinder = new ArrayList<Kind>();
+//	private ArrayList<Gruppe> gruppen = new ArrayList<Gruppe>();
+//	private ArrayList<Kind> kinder = new ArrayList<Kind>();
 	private Gruppe dummiGroup = new GruppeImpl("",9999,"nachts");
 	private Kita dummiKita = new KitaImpl("",9999);
 	private Kind dummiKid = new KindImpl("","",0,9999);
@@ -68,6 +73,9 @@ public class Gui {
 	private boolean warteschlange =false;
 	final JButton btnPrüfenEintragen = new JButton("Pr\u00FCfen");
 	JLabel Preis = new JLabel("");
+	private JTextField textFieldMitglieder;
+	private JTextField textFieldDatum;
+	private JTextField textFieldDauer;
 
 	/**
 	 * Launch the application.
@@ -98,57 +106,58 @@ public class Gui {
 	public Gui() {
 		l = new LogicImpl();
 		
-		
-		kitas.add(new KitaImpl("test1",0));
-		kitas.add(new KitaImpl("test2",1));
-		gruppen.add(new GruppeImpl("testg1",0,"nachts"));
-		gruppen.add(new GruppeImpl("testg2",1,"nachts"));
-		kinder.add(new KindImpl("testk1","nn1",12.000,0));
-		kinder.add(new KindImpl("testk2","nn2",1000000.00,1));
+		kitas.addAll(l.getKitas().values());
+//		kitas.add(new KitaImpl("test1",0));
+//		kitas.add(new KitaImpl("test2",1));
+//		gruppen.add(new GruppeImpl("testg1",0,"nachts"));
+//		gruppen.add(new GruppeImpl("testg2",1,"nachts"));
+//		kinder.add(new KindImpl("testk1","nn1",12.000,0));
+//		kinder.add(new KindImpl("testk2","nn2",1000000.00,1));
 		
 	/**
 	 * Initialize the contents of the frame.
 	 */
 		frame = new JFrame();
-		frame.setBounds(100, 100, 319, 395);
+		frame.setBounds(100, 100, 319, 458);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JLabel lblVorname = new JLabel("Vorname");
-		lblVorname.setBounds(10, 14, 46, 14);
+		lblVorname.setBounds(9, 18, 46, 14);
 		frame.getContentPane().add(lblVorname);
 		
 		JLabel lblNachname = new JLabel("Nachname");
-		lblNachname.setBounds(106, 14, 86, 14);
+		lblNachname.setBounds(106, 18, 86, 14);
 		frame.getContentPane().add(lblNachname);
 		
 		JLabel lblGehaltDerEltern = new JLabel("Gehalt der Eltern");
-		lblGehaltDerEltern.setBounds(205, 14, 86, 14);
+		lblGehaltDerEltern.setBounds(9, 74, 86, 14);
 		frame.getContentPane().add(lblGehaltDerEltern);
 		
 		JLabel lblGruppe_pr = new JLabel("Gruppe");
-		lblGruppe_pr.setBounds(106, 67, 86, 14);
+		lblGruppe_pr.setBounds(105, 130, 86, 14);
 		frame.getContentPane().add(lblGruppe_pr);
 		
 		JLabel lblKita = new JLabel("Kita");
-		lblKita.setBounds(10, 147, 46, 14);
+		lblKita.setBounds(9, 210, 46, 14);
 		frame.getContentPane().add(lblKita);
 		
 		JLabel lblGruppe = new JLabel("Gruppe");
-		lblGruppe.setBounds(10, 193, 46, 14);
+		lblGruppe.setBounds(9, 256, 46, 14);
 		frame.getContentPane().add(lblGruppe);
 		
 		JLabel lblKind = new JLabel("Kind");
-		lblKind.setBounds(10, 242, 46, 14);
+		lblKind.setBounds(9, 305, 46, 14);
 		frame.getContentPane().add(lblKind);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(131, 164, 160, 115);
+		scrollPane.setBounds(130, 227, 160, 115);
 		frame.getContentPane().add(scrollPane);
 		
 		JPanel panel = new JPanel();
 		scrollPane.setViewportView(panel);
 		panel.setLayout(new BorderLayout(0, 0));
+		panel.add(list, BorderLayout.CENTER);
 		
 		list.setModel(new AbstractListModel() {
 			String[] values = new String[] {"1k", "2k", "3k", "4k", "5k", "6k", "7k", "8k", "9k", "10k"};
@@ -159,23 +168,22 @@ public class Gui {
 				return values[index];
 			}
 		});
-		panel.add(list);
+		list.setModel(new DefaultComboBoxModel(kitas.toArray()));
 		
 		final JLabel lblMeldung = new JLabel(" ");
-		lblMeldung.setBounds(10, 116, 282, 23);
+		lblMeldung.setBounds(9, 179, 282, 23);
 		frame.getContentPane().add(lblMeldung);
 		
 		JLabel lblKita_pr = new JLabel("Kita");
-		lblKita_pr.setBounds(10, 67, 46, 14);
+		lblKita_pr.setBounds(9, 130, 46, 14);
 		frame.getContentPane().add(lblKita_pr);
 		
-		comboBoxKitas.setBounds(10, 164, 111, 20);
+		comboBoxKitas.setBounds(9, 227, 111, 20);
 		frame.getContentPane().add(comboBoxKitas);
 //		kitas = l.getKitas();
 		comboBoxKitas.setModel(new DefaultComboBoxModel(kitas.toArray()));
 		comboBoxKitas.insertItemAt(dummiKita, 0);
 		comboBoxKitas.setSelectedIndex(0);
-		list.setModel(new DefaultComboBoxModel(kitas.toArray()));
 		comboBoxKitas.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -193,10 +201,10 @@ public class Gui {
 						
 						comboBoxGruppen.setEnabled(true);
 						comboBoxKinder.setEnabled(true);
-						comboBoxGruppen.setModel(new DefaultComboBoxModel(gruppen.toArray()));
+						comboBoxGruppen.setModel(new DefaultComboBoxModel(l.getGruppen(currentKita.getId()).values().toArray()));
 						comboBoxGruppen.insertItemAt(dummiGroup, 0);
 						comboBoxGruppen.setSelectedIndex(0);
-						list.setModel(new DefaultComboBoxModel(gruppen.toArray()));
+						list.setModel(new DefaultComboBoxModel(l.getGruppen(currentKita.getId()).values().toArray()));
 					}else if(currentKita == dummiKita){
 						list.setModel(new DefaultComboBoxModel(kitas.toArray()));
 					}
@@ -204,7 +212,7 @@ public class Gui {
 			}
 		});
 		
-		comboBoxGruppen.setBounds(10, 211, 111, 20);
+		comboBoxGruppen.setBounds(9, 274, 111, 20);
 		comboBoxGruppen.setEnabled(false);
 		frame.getContentPane().add(comboBoxGruppen);
 		comboBoxGruppen.addActionListener(new ActionListener() {
@@ -220,18 +228,18 @@ public class Gui {
 //						gruppen = l.getGruppen(currentKita.getId());
 						
 						comboBoxKinder.setEnabled(true);
-						comboBoxKinder.setModel(new DefaultComboBoxModel(kinder.toArray()));
+						comboBoxKinder.setModel(new DefaultComboBoxModel(l.getKinder(currentGroup.getId()).values().toArray()));
 						comboBoxKinder.insertItemAt(dummiKid, 0);
 						comboBoxKinder.setSelectedIndex(0);
-						list.setModel(new DefaultComboBoxModel(kinder.toArray()));
+						list.setModel(new DefaultComboBoxModel(l.getKinder(currentGroup.getId()).values().toArray()));
 					}else if(currentGroup == dummiGroup){
-						list.setModel(new DefaultComboBoxModel(gruppen.toArray()));
+						list.setModel(new DefaultComboBoxModel(l.getGruppen(currentKita.getId()).values().toArray()));
 					}
 				}
 			}
 		});
 		
-		comboBoxKinder.setBounds(10, 259, 111, 20);
+		comboBoxKinder.setBounds(9, 322, 111, 20);
 		comboBoxKinder.setEnabled(false);
 		frame.getContentPane().add(comboBoxKinder);
 		comboBoxKinder.addActionListener(new ActionListener() {
@@ -243,13 +251,13 @@ public class Gui {
 					if(currentChild != null && currentChild != dummiKid){
 						list.setModel(new DefaultComboBoxModel(currentChild.getStats()));
 					}else if(currentChild == dummiKid){
-						list.setModel(new DefaultComboBoxModel(kinder.toArray()));
+						list.setModel(new DefaultComboBoxModel(l.getKinder(currentGroup.getId()).values().toArray()));
 					}
 				}
 			}
 		});
 		
-		comboBoxKita_eintr.setBounds(10, 85, 86, 20);
+		comboBoxKita_eintr.setBounds(9, 148, 86, 20);
 		frame.getContentPane().add(comboBoxKita_eintr);
 		comboBoxKita_eintr.setModel(new DefaultComboBoxModel(kitas.toArray()));
 		comboBoxKita_eintr.insertItemAt(dummiKita, 0);
@@ -268,7 +276,7 @@ public class Gui {
 //						gruppen = l.getGruppen(currentKita.getId());
 						
 						comboBoxGruppe_eintr.setEnabled(true);
-						comboBoxGruppe_eintr.setModel(new DefaultComboBoxModel(gruppen.toArray()));
+						comboBoxGruppe_eintr.setModel(new DefaultComboBoxModel(l.getGruppen(currentKita.getId()).values().toArray()));
 						comboBoxGruppe_eintr.insertItemAt(dummiGroup, 0);
 						comboBoxGruppe_eintr.setSelectedIndex(0);
 						currentGroup_eintr = dummiGroup;
@@ -277,7 +285,7 @@ public class Gui {
 			}
 		});
 		
-		comboBoxGruppe_eintr.setBounds(106, 85, 86, 20);
+		comboBoxGruppe_eintr.setBounds(105, 148, 86, 20);
 		frame.getContentPane().add(comboBoxGruppe_eintr);
 		comboBoxGruppe_eintr.addActionListener(new ActionListener() {
 			@Override
@@ -292,7 +300,7 @@ public class Gui {
 		});
 		
 		textFieldVorname = new JTextField();
-		textFieldVorname.setBounds(10, 36, 86, 20);
+		textFieldVorname.setBounds(10, 43, 86, 20);
 		frame.getContentPane().add(textFieldVorname);
 		textFieldVorname.setColumns(10);
 		textFieldVorname.addKeyListener(new KeyListener() {
@@ -311,7 +319,7 @@ public class Gui {
 		});
 		
 		textFieldNachname = new JTextField();
-		textFieldNachname.setBounds(106, 36, 86, 20);
+		textFieldNachname.setBounds(106, 43, 86, 20);
 		frame.getContentPane().add(textFieldNachname);
 		textFieldNachname.setColumns(10);
 		textFieldNachname.addKeyListener(new KeyListener() {
@@ -330,7 +338,7 @@ public class Gui {
 		});
 		
 		textFieldGehalt = new JTextField();
-		textFieldGehalt.setBounds(205, 36, 86, 20);
+		textFieldGehalt.setBounds(9, 99, 86, 20);
 		frame.getContentPane().add(textFieldGehalt);
 		textFieldGehalt.setColumns(10);
 		textFieldGehalt.addKeyListener(new KeyListener() {
@@ -350,7 +358,7 @@ public class Gui {
 		
 		
 		JButton btnRechnungDrucken = new JButton("Rechnung Drucken");
-		btnRechnungDrucken.setBounds(10, 323, 123, 23);
+		btnRechnungDrucken.setBounds(9, 386, 123, 23);
 		frame.getContentPane().add(btnRechnungDrucken);
 		btnRechnungDrucken.addActionListener(new ActionListener() {
 			@Override
@@ -366,7 +374,7 @@ public class Gui {
 		
 		
 		JButton btnPreisErmitteln = new JButton("Preis Ermitteln");
-		btnPreisErmitteln.setBounds(10, 290, 123, 23);
+		btnPreisErmitteln.setBounds(9, 353, 123, 23);
 		frame.getContentPane().add(btnPreisErmitteln);
 		btnPreisErmitteln.addActionListener(new ActionListener() {
 			@Override
@@ -379,29 +387,76 @@ public class Gui {
 			}
 		});
 		
-		Preis.setBounds(146, 290, 145, 23);
+		Preis.setBounds(142, 353, 145, 23);
 		frame.getContentPane().add(Preis);
 		
 		
 		
-		btnPrüfenEintragen.setBounds(202, 84, 89, 23);
+		btnPrüfenEintragen.setBounds(201, 147, 89, 23);
 		frame.getContentPane().add(btnPrüfenEintragen);
+		
+		JLabel lblAnzahl = new JLabel("Familienmitglieder");
+		lblAnzahl.setBounds(106, 74, 89, 14);
+		frame.getContentPane().add(lblAnzahl);
+		
+		textFieldMitglieder = new JTextField();
+		textFieldMitglieder.setColumns(10);
+		textFieldMitglieder.setBounds(106, 99, 86, 20);
+		frame.getContentPane().add(textFieldMitglieder);
+		
+		JLabel lblGeburtsdatum = new JLabel("Geburtsdatum");
+		lblGeburtsdatum.setBounds(202, 18, 101, 14);
+		frame.getContentPane().add(lblGeburtsdatum);
+		
+		textFieldDatum = new JTextField();
+		textFieldDatum.setColumns(10);
+		textFieldDatum.setBounds(202, 43, 86, 20);
+		frame.getContentPane().add(textFieldDatum);
+		
+		JLabel lblBetreuung = new JLabel("Betreuung");
+		lblBetreuung.setBounds(201, 74, 89, 14);
+		frame.getContentPane().add(lblBetreuung);
+		
+		textFieldDauer = new JTextField();
+		textFieldDauer.setHorizontalAlignment(SwingConstants.RIGHT);
+		textFieldDauer.setText("...in Stunden");
+		textFieldDauer.setColumns(10);
+		textFieldDauer.setBounds(201, 99, 86, 20);
+		frame.getContentPane().add(textFieldDauer);
 		btnPrüfenEintragen.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				if(e.getActionCommand() == "Prüfen" || e.getActionCommand() == "Eintragen"){
-
+					DateFormat formatter;
+					Date date;
+					Calendar calendar = null;
 					if(!geprüft){
 						try{
-						if(textFieldVorname.getText() != null && textFieldVorname.getText() != " " && textFieldNachname.getText() != null && textFieldNachname.getText() != " " && Double.parseDouble(textFieldGehalt.getText()) >= 0){
+							try{	
+								formatter = new SimpleDateFormat("MM.dd.yy");
+								date = formatter.parse(textFieldDatum.getText());
+								calendar = Calendar.getInstance();
+								calendar.setTime(date);
+							}catch(Exception ex){
+								geprüft = false;
+								lblMeldung.setText("Ungültiges Datum");
+							}
+							
+						if(textFieldVorname.getText() != null && textFieldVorname.getText() != "" &&
+								textFieldNachname.getText() != null && textFieldNachname.getText() != "" &&
+								Double.parseDouble(textFieldGehalt.getText()) >= 0 && 
+								Integer.parseInt(textFieldMitglieder.getText()) >= 0 && 
+								Integer.parseInt(textFieldDauer.getText()) >= 0){
 							if(currentKita_eintr.getId() != 9999 && currentGroup_eintr.getId() != 9999){
+								double preis = l.preisErmitteln(Integer.parseInt(textFieldMitglieder.getText()), Double.parseDouble(textFieldGehalt.getText()), Integer.parseInt(textFieldDauer.getText()));
 								if(l.isPlatzFrei(currentKita_eintr.getId(),currentGroup_eintr.getId())){
+									
 									geprüft = true;
-									lblMeldung.setText("Platz verfügbar.");
+									lblMeldung.setText("Preis: " + preis + "€. Platz verfügbar.");
 									warteschlange = false;
 								}else{
 									geprüft = true;
-									lblMeldung.setText("Kein Platz verfügbar, Kind in Warteschlange einreihen?");
+									lblMeldung.setText("Preis: " + preis + "€. Kein Platz verfügbar, Kind in Warteschlange einreihen?");
 									warteschlange = true;
 								}
 							}else{
@@ -418,17 +473,25 @@ public class Gui {
 							btnPrüfenEintragen.setText("Pr\u00FCfen");
 						return;
 						}catch(Exception ec){
+							geprüft = false;
 							lblMeldung.setText("Ungültige Angaben");
 						}
 					}
 					
 					
 					if(geprüft){
-						l.kindEintragen(textFieldVorname.getText(),textFieldNachname.getText(),currentKita_eintr.getId(), currentGroup_eintr.getId(), warteschlange,Double.parseDouble(textFieldGehalt.getText()));
-						warteschlange = false;
-						geprüft = false;
-						btnPrüfenEintragen.setText("Pr\u00FCfen");
-						lblMeldung.setText("Kind Eingetragen.");
+						if(l.isPlatzFrei(currentKita_eintr.getId(),currentGroup_eintr.getId())){
+							l.kindEintragen(textFieldVorname.getText(),textFieldNachname.getText(),calendar,currentGroup_eintr.getId(), warteschlange,Double.parseDouble(textFieldGehalt.getText()),Integer.parseInt(textFieldMitglieder.getText()));
+							warteschlange = false;
+							geprüft = false;
+							btnPrüfenEintragen.setText("Pr\u00FCfen");
+							lblMeldung.setText("Kind Eingetragen.");
+						}else{
+							lblMeldung.setText("Platz nicht mehr verfügbar");
+							geprüft = false;
+							btnPrüfenEintragen.setText("Pr\u00FCfen");
+							
+						}
 					}
 				}
 				
