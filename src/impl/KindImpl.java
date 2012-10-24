6965294472e9f7ee53d8af0d76f3a70c2521fd00
anton <anton@dual-core>
2012-10-24 15:42:27 +0200

    Bundesland + gehalt

--- src/impl/KindImpl.java
+++ src/impl/KindImpl.java
@@ -9,17 +9,15 @@ public class KindImpl implements Kind{
 
 	private String vorname;
 	private String nachname;
-	private double gehalt;
 	private Integer id;
 	private Integer familie;
         private Calendar geburtsdatum;
         private Elternteil elternteil;
 	
 
-	public KindImpl(String vorname,String nachname,Calendar geburtsdatum, double gehaltEltern, int id, int familie, Elternteil e) {
+	public KindImpl(String vorname,String nachname,Calendar geburtsdatum, int id, int familie, Elternteil e) {
 		this.vorname = vorname;
 		this.nachname = nachname;
-		this.gehalt = gehaltEltern;
 		this.id = id;
 		this.familie = familie;
                 this.geburtsdatum = geburtsdatum;
@@ -38,10 +36,6 @@ public class KindImpl implements Kind{
 	@Override
 	public Integer getId() {
 		return id;
-	}
-	@Override
-	public double getGehalt() {
-		return gehalt;
 	}
         
         @Override
